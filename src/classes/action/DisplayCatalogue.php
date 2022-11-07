ec9c92974cd74525622d7d01165401acefb4b7d0
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-11-07 14:39:32 +0100

    DisplayCatalogue

---
+++ src/classes/action/DisplayCatalogue.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace iutnc\netvod\action;
+
+use iutnc\netvod\db\ConnectionFactory;
+
+class DisplayCatalogue extends Action
+{
+
+    public function execute(): string
+    {
+        // Variable containing the result
+        $html = "";
+
+        // Connection with the db
+        $query = "SELECT img, titre FROM serie";
+        $stmt = ConnectionFactory::makeConnection();
+        $stmt->prepare($query);
+        $stmt->execute();
+
+        while ($data = $stmt->fetch()){
+            $html = $html . $data["img"] . $data["titre"];
+        }
+
+        return $html;
+    }
+}
