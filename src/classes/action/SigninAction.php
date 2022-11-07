8e762bc04c6537e502a6a0e7362e2a0b66c5f5d8
CochonHalal <ronan.cunin7@etu.univ-lorraine.fr>
2022-11-07 15:19:38 +0100

    SigninAction à tester

--- src/classes/action/SigninAction.php
+++ src/classes/action/SigninAction.php
@@ -2,17 +2,28 @@
 
 namespace iutnc\netvod\action;
 
+use iutnc\netvod\auth\Auth;
+
 class SigninAction extends Action {
 
     public function execute() : string {
         $html = "";
-        if($_SERVER['REQUEST_METHOD'] === "GET") {
+        if($this->http_method === "GET") {
             $html = "
                 <form method='post'>
                     <label>Email: </label><input type='text' name='email' placeholder='toto@gmail.com' required>
                     <label>Password: </label><input type='password' name='password' placeholder='example' required>
                     <button type='submit'>Validate</button>
                 </form>";
+        }
+        elseif ($this->http_method === "POST") {
+            $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+            $passwd = filter_var($_POST['password']);
+            if (Auth::authenticate($email, $passwd)) {
+                $html = "Youre connected";
+            } else {
+                $html = "Your email or password is incorrect";
+            };
         }
         return $html;
     }
