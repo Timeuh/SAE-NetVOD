1d5c8c1910ee1fb4da4efab204b624d77b3c65ce
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-11-07 11:41:54 +0100

    epidose

--- src/classes/appli/Episode.php
+++ src/classes/appli/Episode.php
@@ -4,5 +4,30 @@ namespace iutnc\netvod\appli;
 
 class Episode
 {
+    protected $numero;
+    protected $titre;
+    protected $resume;
+    protected $duree;
+    protected $file;
+    protected $serieId;
+
+    public function __construct($num, $titre, $resume, $duree, $file, $serieId)
+    {
+        $this->numero=$num;
+        $this->titre=$titre;
+        $this->resume=$resume;
+        $this->duree=$duree;
+        $this->file=$file;
+        $this->serieId=$serieId;
+    }
+
+    public function __set(string $name, $value): void {
+        if (property_exists($this, $name)) $this->$name = $value;
+    }
+
+    public function __get(string $name) : mixed{
+        if (isset($this->$name)) return $this->$name;
+        return null;
+    }
 
 }
