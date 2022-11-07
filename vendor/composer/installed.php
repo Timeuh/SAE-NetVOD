1a80c426421789d841eeec9026851084a317e86c
tbrin <timotdu54@gmail.com>
2022-11-07 10:04:13 +0100

    Ajout de composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sae/netvod',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dba2d154729a3a2321d999f2f94cea46920df190',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sae/netvod' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dba2d154729a3a2321d999f2f94cea46920df190',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
