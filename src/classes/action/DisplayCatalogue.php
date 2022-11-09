03ef9b2039dc15b44d5169bf5f90bcff7566eb1e
tbrin <timotdu54@gmail.com>
2022-11-09 14:52:28 +0100

    ajout du tri par nombre d'épisodes

--- src/classes/action/DisplayCatalogue.php
+++ src/classes/action/DisplayCatalogue.php
@@ -3,15 +3,21 @@
 namespace iutnc\netvod\action;
 
 use iutnc\netvod\db\ConnectionFactory;
-use iutnc\netvod\auth\Auth;
 
 class DisplayCatalogue extends Action {
 
     public function execute(): string {
         if ($this->http_method == "GET") {
-            // Variable containing the result
-            $html = "<a href='?action='> Accueil </a> <br> <br>
-                      <form method='post' action='?action=rechercher'>
+            return $this->display();
+        } else {
+            $sort = $_POST['tri'] ?? "default";
+            return $this->display($sort);
+        }
+    }
+
+    private function display(string $sort = "default"): string {
+        $html = "<a href='?action='> Accueil </a> <br> <br>
+                     <form method='post' action='?action=rechercher'>
                         <input type='search' id='form_recherche' name='Recherche'
                         placeholder='Rechercher...'
                         aria-label='Recherche une série dans le site'>
@@ -27,14 +33,6 @@ class DisplayCatalogue extends Action {
                         <button type='submit'>Trier</button>
                       </form>";
 
-            return $html . $this->display();
-        } else {
-            $sort = $_POST['tri'] ?? "default";
-            return $this->display($sort);
-        }
-    }
-
-    private function display(string $sort = "default"): string {
         if (($db = ConnectionFactory::makeConnection()) != null) {
             switch ($sort){
                 case 'titre':
@@ -45,6 +43,10 @@ class DisplayCatalogue extends Action {
                     $query = "SELECT id, img, titre FROM serie order by date_ajout";
                     break;
 
+                case 'nbEp':
+                    $query = "select count(id) as nombre, serie_id from episode group by serie_id order by nombre desc";
+                    break;
+
                 default:
                     $query = "SELECT id, img, titre FROM serie";
                     break;
@@ -52,23 +54,26 @@ class DisplayCatalogue extends Action {
 
             $req = $db->prepare($query);
             $req->execute();
-            $html = "<a href='?action='> Accueil </a> <br> <br>
-                      <form method='post' action='?action=displayCatalogue'>
-                        <select name='tri'>
-                            <option value='select' selected='selected'>Trier le catalogue</option>
-                            <option value='titre'>Trier par titre</option>
-                            <option value='dateAjout'>Trier par date d'ajout</option>
-                            <option value='nbEp'>Trier par nombre d'épisodes</option>
-                        </select>
-                        <button type='submit'>Trier</button>
-                      </form>";
 
-            while ($data = $req->fetch()) {
-                $html = $html . "<img class='img-serie' src='" . "img/" . $data["img"] . "' width='150' height='150'>
+            if ($sort != "nbEp"){
+                while ($data = $req->fetch()) {
+                    $html = $html . "<img class='img-serie' src='" . "img/" . $data["img"] . "' width='150' height='150'>
                         <a href='?action=displaySerie&id=" . $data["id"] . "'>" . $data["titre"] . "</a> <br>";
+                }
+                return $html;
+            } else {
+                while ($data = $req->fetch()) {
+                    $qr = $db->prepare("SELECT id, img, titre FROM serie where id = :id");
+                    $qr->bindParam(':id', $data['serie_id']);
+                    $qr->execute();
+
+                    $res = $qr->fetch();
+                    $html = $html . "<img class='img-serie' src='" . "img/" . $res["img"] . "' width='150' height='150'>
+                        <a href='?action=displaySerie&id=" . $res["id"] . "'>" . $res["titre"] . "</a> <br>";
+                }
+                return $html;
             }
-            return $html;
         }
-        return "";
+        return "<h1>Erreur de base de données</h1><br><a href='Index.php'>Accueil</a>";
     }
 }
