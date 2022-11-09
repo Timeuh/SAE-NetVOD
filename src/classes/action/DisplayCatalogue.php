945c44dd09e41950f355a3c3a35be859fc0a2d38
tbrin <timotdu54@gmail.com>
2022-11-09 14:07:28 +0100

    extraction de l'affichage, pour modifier en fonction du tri

--- src/classes/action/DisplayCatalogue.php
+++ src/classes/action/DisplayCatalogue.php
@@ -5,11 +5,10 @@ namespace iutnc\netvod\action;
 use iutnc\netvod\db\ConnectionFactory;
 use iutnc\netvod\auth\Auth;
 
-class DisplayCatalogue extends Action
-{
+class DisplayCatalogue extends Action {
 
     public function execute(): string {
-        if ($this->http_method == "GET"){
+        if ($this->http_method == "GET") {
             // Variable containing the result
             $html = "<a href='?action='> Accueil </a> <br> <br>
                       <form method='post' action='?action=displayCatalogue'>
@@ -19,24 +18,30 @@ class DisplayCatalogue extends Action
                             <option value='dateAjout'>Trier par date d'ajout</option>
                             <option value='nbEp'>Trier par nombre d'épisodes</option>
                         </select>
+                        <button type='submit'>Trier</button>
                       </form>";
 
-            $db = ConnectionFactory::makeConnection();
-            if ($db != null) {
-                // Connection with the db
-                $query = "SELECT id, img, titre FROM serie";
-                $stmt = $db->prepare($query);
-                $stmt->execute();
-
-                while ($data = $stmt->fetch()) {
-                    $html = $html . "<img class='img-serie' src='" . "img/" . $data["img"]. "' width='150' height='150'>
-                        <a href='?action=displaySerie&id=" . $data["id"] . "'>" . $data["titre"] . "</a> <br>";
-                }
-                return $html;
-            }
-            return $html . "Catalogue introuvable";
+            return $html . $this->display();
         } else {
             return "";
         }
+    }
+
+    private function display(string $sort = "default"): string {
+        $db = ConnectionFactory::makeConnection();
+        if ($db != null) {
+            // Connection with the db
+            $query = "SELECT id, img, titre FROM serie";
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            $html = "";
+
+            while ($data = $stmt->fetch()) {
+                $html = $html . "<img class='img-serie' src='" . "img/" . $data["img"] . "' width='150' height='150'>
+                        <a href='?action=displaySerie&id=" . $data["id"] . "'>" . $data["titre"] . "</a> <br>";
+            }
+            return $html;
+        }
+        return "";
     }
 }
