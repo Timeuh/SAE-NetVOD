0c03336ca7089e4be3979ed42cf770ce142ec22a
tbrin <timotdu54@gmail.com>
2022-11-09 14:19:22 +0100

    tri par titre et date d'ajout fonctionnels

--- src/classes/action/DisplayCatalogue.php
+++ src/classes/action/DisplayCatalogue.php
@@ -23,20 +23,41 @@ class DisplayCatalogue extends Action {
 
             return $html . $this->display();
         } else {
-            return "";
+            $sort = $_POST['tri'] ?? "default";
+            return $this->display($sort);
         }
     }
 
     private function display(string $sort = "default"): string {
-        $db = ConnectionFactory::makeConnection();
-        if ($db != null) {
-            // Connection with the db
-            $query = "SELECT id, img, titre FROM serie";
-            $stmt = $db->prepare($query);
-            $stmt->execute();
-            $html = "";
+        if (($db = ConnectionFactory::makeConnection()) != null) {
+            switch ($sort){
+                case 'titre':
+                    $query = "SELECT id, img, titre FROM serie order by titre";
+                    break;
 
-            while ($data = $stmt->fetch()) {
+                case 'dateAjout':
+                    $query = "SELECT id, img, titre FROM serie order by date_ajout";
+                    break;
+
+                default:
+                    $query = "SELECT id, img, titre FROM serie";
+                    break;
+            }
+
+            $req = $db->prepare($query);
+            $req->execute();
+            $html = "<a href='?action='> Accueil </a> <br> <br>
+                      <form method='post' action='?action=displayCatalogue'>
+                        <select name='tri'>
+                            <option value='select' selected='selected'>Trier le catalogue</option>
+                            <option value='titre'>Trier par titre</option>
+                            <option value='dateAjout'>Trier par date d'ajout</option>
+                            <option value='nbEp'>Trier par nombre d'épisodes</option>
+                        </select>
+                        <button type='submit'>Trier</button>
+                      </form>";
+
+            while ($data = $req->fetch()) {
                 $html = $html . "<img class='img-serie' src='" . "img/" . $data["img"] . "' width='150' height='150'>
                         <a href='?action=displaySerie&id=" . $data["id"] . "'>" . $data["titre"] . "</a> <br>";
             }
