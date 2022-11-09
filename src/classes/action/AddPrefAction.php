ddd5520085c21d6000640fd01c62bc0c1ed1b2c8
Noam Gomes-Odent <gomesode1u@etu.univ-lorraine.fr>
2022-11-09 10:48:06 +0100

    AddPrefAction fini

--- src/classes/action/AddPrefAction.php
+++ src/classes/action/AddPrefAction.php
@@ -9,14 +9,26 @@ class AddPrefAction extends \iutnc\netvod\action\Action
 
     public function execute(): string
     {
-        $serie = $_GET['id'];
+        $idSerie = unserialize($_SESSION['idSerie']);
         $user = unserialize($_SESSION['user']);
+        $idUser = $user->__get('id');
+
         if(($bd = ConnectionFactory::makeConnection())!=null){
-            $insert = $bd->prepare("insert into serieprefuser(idUser, idSerie) values (?,?)");
-            $insert->bindParam(1,$user->id);
-            $insert->bindParam(2,$serie);
-            if($insert->execute()) return "<p>Votre série a bien été ajoutée à votre liste des préférences</p><br><a href='?action=displayCatalogue'>Retour au catalogue</a></br>";
+            $query = $bd->prepare("select idSerie from serieprefuser where idUser = ? and idSerie = ?");
+            $query->bindParam(1, $idUser);
+            $query->bindParam(2, $idSerie);
+            $query->execute();
+
+            if(!$query->fetch()){
+                $insert = $bd->prepare("insert into serieprefuser(idUser, idSerie) values (?,?)");
+                $insert->bindParam(1,$idUser);
+                $insert->bindParam(2,$idSerie);
+                if($insert->execute()) return "<p>Votre série a bien été ajoutée à votre liste des préférences</p>
+                                                <br><a href='?action=displayCatalogue'>Retour au catalogue</a></br>";
+            }else return "<p>Vous avez déjà ajouté cette série à votre liste des préférences</p>
+                            <br><a href='?action=displayCatalogue'>Retour au catalogue</a></br>";
         }
-        return "<p>Il y a eu un problème lors de l'ajout de la série dans votre liste des préférences, veuillez réessayer</p><br><a href='?action=displaySerie'>Revenir à la page de la série</a></br>";
+        return "<p>Il y a eu un problème lors de l'ajout de la série dans votre liste des préférences,
+                veuillez réessayer</p><br><a href='?action=displayCatalogue'>Retour au catalogue</a></br>";
     }
 }
