970f37b3ff7b14a7d6ecfb91b0a4f3d0b30d942f
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-11-10 08:49:21 +0100

    activer compte action

--- src/classes/action/ActiverCompteAction.php
+++ src/classes/action/ActiverCompteAction.php
@@ -2,13 +2,29 @@
 
 namespace iutnc\netvod\action;
 
+use iutnc\netvod\db\ConnectionFactory;
+
 class ActiverCompteAction extends Action
 {
 
     public function execute(): string
     {
-        $html ="";
-        
+        $html ="<a href='?action='>Accueil</a> <br> <br>";
+
+        $db = ConnectionFactory::makeConnection();
+
+        if ($db != null){
+            if ($this->http_method === "GET"){
+
+                $html .= "<form method='post'> 
+                                <button type='submit'>Activer le compte</button>
+                          </form>";
+
+            } elseif($this->http_method === "POST"){
+
+            }
+        }
+
 
     }
 
