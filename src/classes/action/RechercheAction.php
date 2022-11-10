004ab3b152e7b87a7a9cb93ea6648258e6f65a1b
CochonHalal <ronan.cunin7@etu.univ-lorraine.fr>
2022-11-10 09:55:44 +0100

    update CSS

--- src/classes/action/RechercheAction.php
+++ src/classes/action/RechercheAction.php
@@ -13,7 +13,7 @@ class RechercheAction extends Action
                         <input type='search' id='recherche' name='recherche' class='border-2 rounded-md bg-grey-300 text-black'
                         placeholder='Rechercher...' size='150'>
                         <button type='submit' class='border-2 rounded-md bg-yellow-500 border-yellow-500 hover:bg-yellow-600'>Rechercher</button>
-                      </form> <br>";
+                      </form> <br> <br>";
         if (($bd = ConnectionFactory::makeConnection()) != null) {
             $titre = "%".filter_var($_POST['recherche'],FILTER_SANITIZE_SPECIAL_CHARS)."%";
             $query = "select id, titre, img from serie where titre like :titre";
@@ -21,7 +21,7 @@ class RechercheAction extends Action
             $get->bindParam(':titre', $titre);
             $get->execute();
             while ($data = $get->fetch()){
-                $html = $html .  "<img class='img-serie' src='" . "img/" . $data["img"]. "'width='400' height='400'> " . "<a href='?action=displaySerie&id=" . $data["id"] . ">" . $data["titre"] . "</a> <br> <br>";
+                $html = $html .  "<img class='img-serie' src='" . "img/" . $data["img"]. "' width='400' height='400'> " . "<a href='?action=displaySerie&id=" . $data["id"] . "' class='text-white'>" . $data["titre"] . "</a> <br> <br>";
             }
         }
         return $html;
