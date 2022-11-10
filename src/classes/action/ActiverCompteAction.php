bf9f82a063954b45243c4cb9f8f55ee8ae8e7cb1
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-11-10 10:12:54 +0100

    activer compte

--- src/classes/action/ActiverCompteAction.php
+++ src/classes/action/ActiverCompteAction.php
@@ -21,10 +21,34 @@ class ActiverCompteAction extends Action
                           </form>";
 
             } elseif($this->http_method === "POST"){
+                $token = unserialize($_SESSION['token']);
 
+                if ($token != null) {
+
+                    $queryGetUser = "SELECT * FROM user WHERE activation_token=:token AND activation_expires > date('Y-m-d H:i:s',time())";
+                    $stmt1 = $db->prepare($queryGetUser);
+                    $stmt1->bindParam('token', $token);
+                    $stmt1->execute();
+
+                    $data = $stmt1->fetch();
+
+                    $queryUpdate = "UPDATE user SET active = 1, activation_token=null WHERE activation_token=:token2";
+                    $stmt2 = $db->prepare($queryUpdate);
+                    $stmt2->bindParam('token2', $token);
+                    $stmt2->execute();
+
+                    $html .= "<script>document.location.href='?action='</script>";
+                } else {
+                    $html .= "Aucun compte ne doit être activé pour le moment <br> <br>
+                              <a href='?action='>Retour à Accueil</a>" ;
+                }
+
+            } else {
+                $html .= "Erreur";
             }
         }
 
+        return $html;
 
     }
 
